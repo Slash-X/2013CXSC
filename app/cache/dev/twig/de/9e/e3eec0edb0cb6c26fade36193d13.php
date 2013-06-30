ef90f390810ada5faefeddb2fcf1c00b11d86262
jhivanhoe <jhivanhoe@Mac-mini-de-jhivanhoe.local>
2013-06-30 22:26:05 +0400

    ...

---
+++ app/cache/dev/twig/de/9e/e3eec0edb0cb6c26fade36193d13.php
@@ -0,0 +1,119 @@
+<?php
+
+/* SlashxAdminBundle:Album:index.html.twig */
+class __TwigTemplate_de9ee3eec0edb0cb6c26fade36193d13 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("SlashxAdminBundle::layout.html.twig");
+
+        $this->blocks = array(
+            'block_title' => array($this, 'block_block_title'),
+            'sub_main' => array($this, 'block_sub_main'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "SlashxAdminBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_block_title($context, array $blocks = array())
+    {
+        // line 4
+        echo "GESTION DES TYPES
+";
+    }
+
+    // line 7
+    public function block_sub_main($context, array $blocks = array())
+    {
+        // line 8
+        echo "    <h1>Album list</h1>
+
+    <table class=\"records_list\">
+        <thead>
+            <tr>
+              
+                <th>Titre</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        ";
+        // line 19
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["entities"]) ? $context["entities"] : $this->getContext($context, "entities")));
+        foreach ($context['_seq'] as $context["_key"] => $context["entity"]) {
+            // line 20
+            echo "        
+            <tr>
+                
+                <td>";
+            // line 23
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "titre"), "html", null, true);
+            echo "-----------</td>
+                <td>
+                <ul>
+                    <li>
+                        <a href=\"";
+            // line 27
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("album_edit", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
+            echo "\">edit</a>
+                    </li>
+                    <li>
+                        <a href=\"";
+            // line 30
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("photographie", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
+            echo "\">photo</a>
+                    </li>
+                </ul>
+                </td>
+            </tr>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['entity'], $context['_parent'], $context['loop']);
+        $context = array_merge($_parent, array_intersect_key($context, $_parent));
+        // line 36
+        echo "        </tbody>
+    </table>
+
+    <p class=\"records_list\">
+        
+\t            <a class='button add' href=\"";
+        // line 41
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("album_new"), "html", null, true);
+        echo "\">
+\t                Ajouter un nouvel album
+\t                </a>
+\t          
+    </p>
+    
+    <p>&nbsp;</p>
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "SlashxAdminBundle:Album:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  94 => 41,  87 => 36,  75 => 30,  69 => 27,  62 => 23,  57 => 20,  53 => 19,  40 => 8,  37 => 7,  32 => 4,  29 => 3,);
+    }
+}
---
+++ src/Slashx/AdminBundle/Form/PapillonType-.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Slashx\AdminBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PapillonType extends AbstractType {
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder->add('famille')->add('espece')
+				->add('sousEspece', 'text', array('required' => false))
+				->add('forme', 'text', array('required' => false))
+				->add('auteur')->add('date')->add('nom')->add('statut')
+				->add('distributions', 'choice',
+						array('multiple' => true,
+								'choices' => array('Cameroun' => 'Cameroun',
+										'Gabon' => 'Gabon', 'Congo' => 'Congo'),
+								'required' => false, 'empty_data' => null))
+				->add('habitats', 'choice',
+						array('multiple' => true,
+								'choices' => array('Cameroun' => 'Cameroun',
+										'Gabon' => 'Gabon', 'Congo' => 'Congo'),
+								'required' => false, 'empty_data' => null))
+				->add('plantes', 'choice',
+						array('multiple' => true,
+								'choices' => array('Cameroun' => 'Cameroun',
+										'Gabon' => 'Gabon', 'Congo' => 'Congo'),
+								'required' => false, 'empty_data' => null))
+				->add('premiersEtats', 'choice',
+						array('multiple' => true,
+								'choices' => array('Cameroun' => 'Cameroun',
+										'Gabon' => 'Gabon', 'Congo' => 'Congo'),
+								'required' => false, 'empty_data' => null))
+				->add('couleur');
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver
+				->setDefaults(
+						array(
+								'data_class' => 'Slashx\AdminBundle\Entity\Papillon'));
+	}
+
+	public function getName() {
+		return 'slashx_adminbundle_papillontype';
+	}
+}
